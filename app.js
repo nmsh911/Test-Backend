36debe840707bdae985c961c002aa6530c2087af
nmsh911 <coding911@gmail.com>
2024-02-29 14:17:51 +0530

    first commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res) => {
+  res.json({message: "I love you"})
+})
+
 app.use("/api/user", userRouter);
 
 app.listen(8080, () => {
